f6ac3c623a3bdb509730b57b33a02379633f353a
zhongyw <zw198307@163.com>
2023-10-24 15:17:13 +0800

    init frontend and backend

---
+++ backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+ 
+DEBUG = True
+ 
+app = Flask(__name__)
+app.config.from_object(__name__)
+ 
+CORS(app, resources={r'/*': {'origins': '*'}})
+ 
+ 
+@app.route('/api/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+ 
+ 
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+ 
+ 
+@app.route('/<path:fallback>')
+def fallback(fallback):       # Vue Router 的 mode 为 'hash' 时可移除该方法
+    if fallback.startswith('css/') or fallback.startswith('js/')\
+            or fallback.startswith('img/') or fallback == 'favicon.ico':
+        return app.send_static_file(fallback)
+    else:
+        return app.send_static_file('index.html')
+ 
+ 
+if __name__ == '__main__':
+    app.run()
